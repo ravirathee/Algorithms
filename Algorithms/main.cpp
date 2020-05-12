96d16695e4f266a61ae020749a7e5d6a3cdb2736
ravirathee <ravik.rathee@gmail.com>
2020-05-12 18:26:34 +0530

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Ravi Rathee on 12/05/20.
+//  Copyright © 2020 Ravi Rathee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
